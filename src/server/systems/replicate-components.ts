a9baad6f28c35ca0530e71c6eff9fc4f8d67750d
Deepsn <75620590+Deepsn@users.noreply.github.com>
2024-06-12 19:34:34 -0300

    feat: replicate existing components to client

--- src/server/systems/replicate-components.ts
+++ src/server/systems/replicate-components.ts
@@ -1,12 +1,7 @@
-import {
-	type ComponentCreator,
-	MatterComponents,
-} from "@/shared/matter/component";
+import { MatterComponents } from "@/shared/matter/component";
 import { remotes } from "@/shared/remotes";
 import { type AnyComponent, type World, useEvent } from "@rbxts/matter";
 import type { RootSystem } from "../runtime.server";
-
-const REPLICATED_COMPONENTS = [] as ComponentCreator[];
 
 function ReplicateComponents(world: World) {
 	const payload = new Map<string, Map<string, AnyComponent>>();
@@ -35,11 +30,13 @@ function ReplicateComponents(world: World) {
 
 	payload.clear();
 
-	for (const component of REPLICATED_COMPONENTS) {
-		for (const [entityId, componentData] of world.queryChanged(component)) {
+	for (const [, { newComponent, settings }] of MatterComponents) {
+		if (!settings.replicable) continue;
+
+		for (const [entityId, componentData] of world.queryChanged(newComponent)) {
 			const key = tostring(entityId);
 			const components = payload.get(key) ?? new Map();
-			const componentId = tostring(component);
+			const componentId = tostring(newComponent);
 
 			if (world.contains(entityId) && componentData.new) {
 				components.set(componentId, componentData.new);
